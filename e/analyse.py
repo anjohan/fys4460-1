9f4129fd34558fbdc8df4308bb8d2fb6bfca159b
anjohan <anjohan@uio.no>
2018-02-10 19:33:09 +0100

    e) ferdig

--- e/analyse.py
+++ e/analyse.py
@@ -1,3 +1,4 @@
+import pdb
 from units import units
 import logplotter
 import tqdm
@@ -11,6 +12,7 @@ num_rhos = len(rhos)
 num_Ts = len(Ts)
 
 Ps = np.zeros((num_rhos, num_Ts))
+equilibrium_Ts = np.zeros_like(Ps)
 
 for i, rho in enumerate(tqdm.tqdm(rhos, desc="Densities:".ljust(20))):
     for j, T in enumerate(tqdm.tqdm(Ts, desc="Temperatures:".ljust(20))):
@@ -21,10 +23,11 @@ for i, rho in enumerate(tqdm.tqdm(rhos, desc="Densities:".ljust(20))):
         k = 0
         while abs(T[-1] - T[k]) > 0.5 * abs(T[0] - T[-1]):
             k += 1
-        equilibrium_index = 4 * k
-        assert equilibrium_index < 0.8 * \
-            num_frames, "Longer simulation needed for rho=%g, T=%g" % (rho, T)
+        equilibrium_index = 3 * k
+        assert equilibrium_index < (0.8 * num_frames)  # ,\
+        # "Longer simulation needed for rho=%g, T=%g" % (rho, T)
         Ps[i, j] = np.mean(P[equilibrium_index:])
+        equilibrium_Ts[i, j] = np.mean(T[equilibrium_index:])
 
 # rhos /= units.L0**3
 # Ts *= units.T0
@@ -35,20 +38,25 @@ print()
 
 def vanderWaal(rhos_and_Ts, a, b):
     rhos, Ts = rhos_and_Ts
-    num_rhos, num_Ts = len(rhos), len(Ts)
-    Ps = np.zeros((num_rhos, num_Ts))
+    num_rhos = len(rhos)
+    num_Ts = int(len(Ts) / len(rhos))
+    Ts = Ts.reshape((num_rhos, num_Ts))
+    Ps = np.zeros_like(Ts)
+    # pdb.set_trace()
     for i, rho in enumerate(rhos):
-        for j, T in enumerate(Ts):
-            Ps[i, j] = rho**2 * (T / (1 - rho * b) - a)
+        for j in range(num_Ts):
+            Ps[i, j] = rho * Ts[i, j] / (1 - rho * b) - a * rho**2
     return Ps.ravel()
 
 
 print("Finding parameters...")
-params, covariancestuff = curve_fit(vanderWaal, [rhos, Ts], flat_Ps)
+params, covariancestuff = curve_fit(
+    vanderWaal, [rhos, equilibrium_Ts.ravel()], flat_Ps, p0=(1.2, 0.1))
+print(params, covariancestuff)
 
 a, b = params
 print("a = %g, b = %g" % (a, b))
-computed_P = vanderWaal([rhos, Ts], a, b)
+computed_P = vanderWaal([rhos, equilibrium_Ts.ravel()], a, b)
 relative_error = np.abs((computed_P - flat_Ps) / flat_Ps)
 
 for filename, values in (("fitted.dat", computed_P.reshape(
@@ -56,7 +64,10 @@ for filename, values in (("fitted.dat", computed_P.reshape(
                                                   relative_error.reshape(
                                                       (num_rhos, num_Ts)))):
     with open("data/" + filename, "w") as outfile:
-        for j, T in enumerate(Ts):
+        for j in range(num_Ts):
             for i, rho in enumerate(rhos):
-                outfile.write("%g %g %g\n" % (rho, T, values[i, j]))
+                outfile.write("%g %g %g\n" % (rho, equilibrium_Ts[i, j],
+                                              values[i, j]))
             outfile.write("\n")
+np.savetxt("data/a.dat", [a])
+np.savetxt("data/b.dat", [b])
--- e/simulate.py
+++ e/simulate.py
@@ -2,8 +2,8 @@ from tqdm import trange
 import os
 import numpy as np
 
-rhos = np.linspace(0.005, 0.025, 20)
-Ts = np.linspace(1.0, 3.0, 20)
+rhos = np.linspace(0.01, 1, 30)
+Ts = np.linspace(1, 3.0, 10)
 
 np.savetxt("data/rhos.dat", rhos)
 np.savetxt("data/Ts.dat", Ts)
